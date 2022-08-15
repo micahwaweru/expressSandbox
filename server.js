1c1b40c93458c8e9e22e0f55abe3fe121b265a0b
micahwaweru <micah.waweru@gmail.com>
2022-08-15 10:45:20 -0500

    monday commit

--- server.js
+++ server.js
@@ -44,9 +44,48 @@ app.get('/read',(req,res)=>{
 })
 
 //update
+app.put('/update/:id',(req,res)=>{
+    var id = req.params.id - 1;
+    console.log(id)
+    
+    
+    fs.readFile('db.json',(err,data)=>{
+        if(err)throw err;
+        dbData = JSON.parse(data);
+        var dataId = dbData[id]
+        dataId.firstName=req.body.firstName
+        dataId.lastName=req.body.lastName
+        stringData = JSON.stringify(dbData)
+        fs.writeFile('db.json',stringData,(err)=>{
+            if(err)throw err
+        })
+
+    })
+    res.send('Note Updated successfully')
+})
 
 //delete
+app.delete('/delete/:id',(req,res)=>{
+    var id = req.params.id - 1;
+
+    fs.readFile('db.json',(err,data)=>{
+        if(err)throw err;
+        dbData = JSON.parse(data);
+        var dataId = dbData[id];
+        for(let i=0;i<dbData.length;i++){
+            if(dbData[i].id===Number(dataId)){
+                dbData.splice([i],1);
+            }
+        }
+        stringData = JSON.stringify(dbData);
+        fs.writeFile('db.json',stringData,(err)=>{
+            if(err)throw err
+        })
+    })
+    res.send(`Data ${id} successfully deleted`)
+})
 
 app.listen(PORT, ()=>{
     console.log(`Example app listen at http://localhost:${PORT}`)
 })
+
